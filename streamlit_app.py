612f5aee0c866f42c8b1e09795e447de0979e2da
2402pooja <125534920+2402pooja@users.noreply.github.com>
2023-02-22 12:38:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-inport snowflake.connector
+import snowflake.connector
